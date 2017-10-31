b006cc3b57153343726b6bcc68f6dbd7610667bf
“Tea <teateearu@gmail.com>
2017-10-31 15:50:26 +0100

    Take the order

--- pizzeria.rb
+++ pizzeria.rb
@@ -15,4 +15,12 @@ def show_menu
   puts "2 - Quattro Fromaggi - #{@pizza_quattro_fromaggi} Euro"
   puts "3 - Margherita - #{@pizza_margherita} Euro"
   puts "4 - Vegetariana - #{@pizza_vegetariana} Euro"
-end 
+end
+
+def take_order
+  print "Enter the number of the pizza as displayed on the menu: "
+  number = gets.chomp
+  print "How many pizzas would you like to order? "
+  count = gets.chomp.to_i
+  return number, count
+end
